90a30d21dd4ae36d169e3a1a84e53d205652e831
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2025-04-07 00:34:58 -0400

    Final update for main.py
    In this update, I finished with the data visualization on a plot graph that displays the data that I previously obtained from the .csv file.

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
 import csv
 import matplotlib.pyplot as plt
 
-def handle_csv_file (filename):
-    population_per_continent = {}
 
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+def handle_csv_file(filename):
+  """
+    This function handles the csv file with the data to be used for the project
+    It opens the file, reads it, and returns a dictionary with all the data.
+    """
 
-        for line in reader:
-            continent = line['continent']
-            year = int(line['year'])
-            population = int(line['population'])
+  population_per_continent = {}
+  
+  # Open CSV file
+  with open(filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
 
-            if continent not in population_per_continent:
-                population_per_continent[continent] = {'year': [], 'population': []}
-                
-            population_per_continent[continent]['year'].append(year)
-            population_per_continent[continent]['population'].append(population)
-            
+    # Read CSV file
+    for line in reader:
+      continent = line['continent']
+      year = int(line['year'])
+      population = int(line['population'])
 
-    return population_per_continent
+      # Store CSV file data in a dictionary
+      if continent not in population_per_continent:
+        population_per_continent[continent] = {'years': [], 'population': []}
 
-def create_data_dictionary(reader):
-    print("In progress")
+      population_per_continent[continent]['years'].append(year)
+      population_per_continent[continent]['population'].append(population)
 
-    
+  return population_per_continent
 
 
-# Open and read csv file
+def display_data_in_plot_graph(population_per_continent_dictionary):
+    """
+    Obtains data from a dictionary and displays it on a plot graph
+    In this case, we are displaying the growth of Internet users in different continents
+    """
+  # Reading data in dictionary
+  for continent in population_per_continent_dictionary:
+    years = population_per_continent_dictionary[continent]['years']
+    population = population_per_continent_dictionary[continent]['population']
+
+    plt.plot(years, population, label=continent, marker='o')
+
+  # General plot graph styling (titles, legend, labels...)
+  plt.title("Growth of Internet Users per Continent")
+  plt.legend()
+  plt.grid()
+  plt.xlabel("Years")
+  plt.ylabel("Population (in Billions)")
+  plt.tight_layout()
+
+  # Visualizing plot graph
+  plt.show()
+
+
+# Open, read and store csv file data in dictionary
 filename = "data.csv"
 population_dictionary = handle_csv_file(filename)
 
-# Store csv file data in dictionary
-
 # Display plot graph
+display_data_in_plot_graph(population_dictionary)
